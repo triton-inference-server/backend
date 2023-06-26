49993f693b48aa4eb14507ec1544eb79b66d3f9b
David Yastremsky <dyastremsky@nvidia.com>
2023-06-26 08:27:50 -0700

    Run pre-install with updated config

--- examples/model_repos/bls_models/addsub_python/1/model.py
+++ examples/model_repos/bls_models/addsub_python/1/model.py
@@ -35,13 +35,9 @@ class TritonPythonModel:
     def initialize(self, args):
         self.model_config = model_config = json.loads(args["model_config"])
 
-        output0_config = pb_utils.get_output_config_by_name(
-            model_config, "OUTPUT0"
-        )
+        output0_config = pb_utils.get_output_config_by_name(model_config, "OUTPUT0")
 
-        output1_config = pb_utils.get_output_config_by_name(
-            model_config, "OUTPUT1"
-        )
+        output1_config = pb_utils.get_output_config_by_name(model_config, "OUTPUT1")
 
         self.output0_dtype = pb_utils.triton_string_to_numpy(
             output0_config["data_type"]
@@ -65,12 +61,8 @@ class TritonPythonModel:
                 in_0.as_numpy() - in_1.as_numpy(),
             )
 
-            out_tensor_0 = pb_utils.Tensor(
-                "OUTPUT0", out_0.astype(output0_dtype)
-            )
-            out_tensor_1 = pb_utils.Tensor(
-                "OUTPUT1", out_1.astype(output1_dtype)
-            )
+            out_tensor_0 = pb_utils.Tensor("OUTPUT0", out_0.astype(output0_dtype))
+            out_tensor_1 = pb_utils.Tensor("OUTPUT1", out_1.astype(output1_dtype))
 
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[out_tensor_0, out_tensor_1]
