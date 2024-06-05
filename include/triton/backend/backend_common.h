78225c56a28097f1b575879014f9f0118a2b5008
krishung5 <krish@nvidia.com>
2024-06-05 16:39:42 -0700

    Add kCUDAExecutionAccelerator constant

--- include/triton/backend/backend_common.h
+++ include/triton/backend/backend_common.h
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright 2020-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -279,6 +279,7 @@ constexpr int WILDCARD_DIM = -1;
 
 constexpr char kTensorRTExecutionAccelerator[] = "tensorrt";
 constexpr char kOpenVINOExecutionAccelerator[] = "openvino";
+constexpr char kCUDAExecutionAccelerator[] = "cuda";
 constexpr char kGPUIOExecutionAccelerator[] = "gpu_io";
 constexpr char kAutoMixedPrecisionExecutionAccelerator[] =
     "auto_mixed_precision";
