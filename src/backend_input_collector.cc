25fcff8487abfca7da50d8de124c705857a691ad
Sai Kiran Polisetty <spolisetty@nvidia.com>
2025-04-24 14:55:22 +0530

    test

--- src/backend_input_collector.cc
+++ src/backend_input_collector.cc
@@ -25,7 +25,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "triton/backend/backend_input_collector.h"
-
+#include <sstream>
 #include <atomic>
 
 #include "triton/backend/backend_common.h"
